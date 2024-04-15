b9e5b69a896c2459264e40fc8ab7dd411983409b
Egemen <egemenc2101@gmail.com>
2024-04-15 16:13:25 +0300

    inital commit with bootstrap and primeng configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { AuthComponent } from './auth/auth.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, ButtonModule, AuthComponent],
+  template: `
+    <div class="card flex justify-content-center">
+      <p-button label="Submit"></p-button>
+      <app-auth></app-auth>
+    </div>
+  `,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'client';
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [],
+  template:`<div class="bg-primary">safdsa</div>`,
+  styleUrl: './auth.component.scss'
+})
+export class AuthComponent {
+
+}
