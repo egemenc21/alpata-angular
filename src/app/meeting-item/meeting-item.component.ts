a451bde0f40db44004161eea0c2a8672de426d1f
Egemen <egemenc2101@gmail.com>
2024-05-03 16:05:45 +0300

    meeting-item updated with primeng components

--- src/app/meeting-item/meeting-item.component.ts
+++ src/app/meeting-item/meeting-item.component.ts
@@ -4,74 +4,52 @@ import { Meeting, MeetingService } from '../services/meeting.service';
 import { ButtonModule } from 'primeng/button';
 import { environment } from '../../environments/environment.development';
 import { RouterLink } from '@angular/router';
-
+import { CardModule } from 'primeng/card';
 @Component({
   selector: 'app-meeting-item',
   standalone: true,
-  imports: [CommonModule, ButtonModule, RouterLink],
-  template: `<li
-    *ngIf="meeting"
-    class="border p-3 rounded-lg d-flex flex-column align-items-center h-100 w-100 "
-  >
-    <div class="p-2">
-      <h3 class="text-center h5">{{ meeting.name | uppercase }}</h3>
-      <div>
-        <span class="text-danger ">Description: </span>{{ meeting.description }}
-      </div>
-      <div>
+  imports: [CommonModule, ButtonModule, RouterLink, CardModule],
+  template: `
+    <p-card header="{{ this.meeting.name | titlecase }}">
+      <p>
+        {{ this.meeting.description }}
+      </p>
+      <p>
         <span class="text-danger ">Start Date:</span>
         {{ meeting.startDate | date }}
-      </div>
-      <div>
+      </p>
+      <p>
         <span class="text-danger ">End Date:</span> {{ meeting.endDate | date }}
-      </div>
-      <div class="">
+      </p>
+      <div>
         <span class="text-danger ">Document Url:</span>
         <a
           href="{{ this.documentHref }}"
           target="_blank"
           class=" document-url "
-          *ngIf="this.meeting.documentUrl !== '' "
+          *ngIf="this.meeting.documentUrl !== ''"
         >
           See Document <i class="pi pi-external-link"></i>
         </a>
       </div>
-    </div>
-    <div class="d-flex flex-column flex-md-row align-items-center w-100">
-      <button
-        pButton
-        pRipple
-        type="button"
-        label="Modify"
-        class="p-button-success w-50"
-        [raised]="true"
-        [rounded]="true"
-        severity="success"
-        routerLink="/dashboard/modify/{{ this.meeting.id }}"
-      ></button>
 
-      <button
-        pButton
-        pRipple
-        type="button"
-        label="Delete"
-        class="p-button-success w-50"
-        [raised]="true"
-        [rounded]="true"
-        severity="danger"
-        (click)="deleteMeeting(meeting.id)"
-      ></button>
-    </div>
-  </li>`,
+      <ng-template pTemplate="footer">
+        <p-button
+          label="Modify"
+          icon="pi pi-file-edit"
+          routerLink="/dashboard/modify/{{ this.meeting.id }}"
+        ></p-button>
+        <p-button
+          label="Delete"
+          icon="pi pi-times"
+          styleClass="p-button-secondary"
+          [style]="{ 'margin-left': '.5em' }"
+          (click)="deleteMeeting(meeting.id)"
+        ></p-button>
+      </ng-template>
+    </p-card>
+  `,
   styleUrl: './meeting-item.component.scss',
-  styles: [
-    `
-      .document-url {
-        text-overflow: ellipsis;
-        white-space: nowrap;
-      }
-    `,
-  ],
 })
 export class MeetingItemComponent {
   @Input() meeting: Meeting;
@@ -79,10 +57,7 @@ export class MeetingItemComponent {
 
   meetingService = inject(MeetingService);
   documentHref: string;
-  constructor() {
-    
-  }
-
+  constructor() {}
 
   ngOnInit() {
     this.documentHref = `${environment.apiRoute}/Uploads/Documents/${this.meeting.documentUrl}`;
